e7758327ecd8855c977a842e6f0517b68d0843f3
Frank Hummel <zkshangxiao@126.com>
2017-02-24 00:58:42 +0800

    5a 添加数据库关系

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
+import os
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_moment import Moment
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -18,6 +27,26 @@ moment = Moment(app)
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+    users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
 @app.errorhandler(404)
